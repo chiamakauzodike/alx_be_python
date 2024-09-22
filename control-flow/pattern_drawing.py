327d616fdac661b8eae7d2253f7e530b816b19dc
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-22 15:59:15 -0700

    third answer

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,12 @@
 #nested pattern
+size = int(input("Enter the size of the pattern: "))
+row = 1
+symbols =["*", "*", "*", "*", "*"]
+while row <= size:
+    for col in range(size):
+        if col == row - 1:
+            print(symbols[row], end="")
+        else:
+            print(symbols[1], end="")
+    print()
+    row += 1
