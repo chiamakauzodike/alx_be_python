7c6016cd0da8c6e782bdf5b048016377deb25deb
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-29 15:53:01 -0700

    update temp conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 #temperatur conversion tool
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-eELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celcius
