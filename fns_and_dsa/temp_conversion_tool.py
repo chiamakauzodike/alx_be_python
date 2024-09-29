db579d2c0c90fae93fb6003099ba5b0a8a3926c8
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-29 15:17:01 -0700

    adding temperature conversion tool using global scope

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,1 +1,23 @@
 #temperatur conversion tool
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_OFFSET = 32
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+def convert_to_fahrenheit(celsius):
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + FAHRENHEIT_OFFSET
+    return fahrenheit
+def main():
+    temperature = float(input("Enter the temperature to convert: ")
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    if unit == "F":
+        celsius = convert_to_celsius(temperature)
+        print(f"{temperature}°F is {celcius:.2f}°C")
+    elif unit =="C":
+        fahrenheit = convert_to_fahrenheit(temperature)
+        print(f"{temperature}°C is {fahrenheit:.2f}°F)
+    else:
+        print(f"Invalid temperature. Please enter a numeric value.")
+
+
