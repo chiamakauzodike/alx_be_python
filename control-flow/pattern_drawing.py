9c38dd8a9d8e70aad0acf2447ffb60d06d51a3e4
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-28 18:27:23 -0700

    updated pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -7,6 +7,7 @@ while row <= size:
         if col == row - 1:
             print(symbols[row], end="")
         else:
-            print(symbols[1], end="")
+            print("*", end="")
+           # print(symbols[1], end="")
     print()
     row += 1
