93aeb98acd761798eb152d2e7cfebeb5ae9d7b78
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-28 17:07:01 -0700

    adding answers tp perform operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 #arithmetic operator
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if opation =='add':
         return num1 + num2
     elif operation == 'subtract':
