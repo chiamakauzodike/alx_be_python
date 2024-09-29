60fbeb90053c5d85ced1e8fd9221b5c71b51926b
Chi <chiamakauzodike@users.noreply.github.com>
2024-09-28 18:42:34 -0700

    adding datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,6 @@
+#python daytime
+from datetime import daytime, timedelta
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,1 @@
+#temperatur conversion tool
