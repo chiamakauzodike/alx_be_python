1bf170dc6c76f926f10852ac55200ba5ef94da63
Chi <chiamakauzodike@users.noreply.github.com>
2024-10-05 18:58:30 -0700

    testing method answers

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -22,11 +22,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(2, 3), 5)
 
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
 
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(2, 3), 5)
         self.assertEqual(self.calc.divide(-2, -3), -5)
         with self.assertionRaise(ValueError):
