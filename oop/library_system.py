37ae8f291e78cee36bd50f9434f1639b13e223d8
Chi <chiamakauzodike@users.noreply.github.com>
2024-10-08 15:58:59 -0700

    adding inheritance and composition update

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,7 +6,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author} (EBook, {self.file}MB)"
+        return f"EBook: {self.title} by {self.author} File Size: {self.file_size}KB)"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -22,7 +22,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{self.title} by {self.author} (Print, {page_count} pages)"
+        return f"PrintBook: {self.title} by {self.author} Page Count: {page_count}"
 
 class Library:
     def __init__(self):
