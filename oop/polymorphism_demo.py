65aaec89af70e29d379df8d7f08f2885d00f3fa3
Chi <chiamakauzodike@users.noreply.github.com>
2024-10-11 17:53:40 -0700

    addig the polymorphism demo answer

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -11,7 +11,7 @@ class Rectangle(Shape):
         self.width = width
         
     def area(self):
-        return self.length 8 self.width
+        return self.length * self.width
 
 class Circle(Shape):
     def __init__(self, radius):
